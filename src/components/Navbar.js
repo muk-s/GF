c3f4e3e4d8b52d3ee983d594ab435c13127e3655
muk-p <jmukara959@gmail.com>
2025-07-23 16:34:50 +0300

    commits

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,9 +1,10 @@
 import { Link } from 'react-router-dom';
 import { useCart } from '../context/CartContext';
+import { useNavigate } from 'react-router-dom';
 
 function Navbar() {
   const { cart } = useCart();
-
+  const navigate = useNavigate();
   return (
     <nav className="bg-primary text-white p-4">
       <div className="container mx-auto flex justify-between items-center">
@@ -11,10 +12,10 @@ function Navbar() {
           MyShop
         </Link>
         <div className="flex items-center space-x-4">
-          <Link to="/" className="hover:underline">
+          <Link to="/" className="hover:underline" onClick={() => navigate('/')}>
             Home
           </Link>
-          <Link to="/cart" className="hover:underline">
+          <Link to="/cart" className="hover:underline" onClick={() => navigate('/cart')}>
             Cart ({cart.length})
           </Link>
         </div>
